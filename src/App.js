59ec8676c465737b50378ca39652290e366f10ac
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-13 09:12:19 +0500

    added footer comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Headers from "./components/Headers";
 
 function App() {
   return (
     <div>
       <Headers />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css';
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>&copy; 2023 My Gaming Website</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
