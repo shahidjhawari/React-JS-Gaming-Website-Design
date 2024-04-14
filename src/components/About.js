e902fae3427edbef7d8e2ce0f15baba2de90f396
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-14 09:54:58 +0500

    latest

--- src/components/About.js
+++ src/components/About.js
@@ -2,36 +2,65 @@ import React from "react";
 import "./styles.css";
 import Footer from "./Footer";
 import Headers from "./Headers";
+import shahid from './img/shahid.png';
+import muutafa from './img/muutafa.jpg';
+import ali from './img/ali.jpg';
 
 function About() {
   return (
     <div>
       <Headers />
       <main className="abo">
-        <section className="contact-form">
-          <h1 className="h1">Contact Us</h1>
-          <form>
-            <div className="form-group">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="email"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <textarea
-                id="message"
-                name="message"
-                rows="4"
-                placeholder="message"
-                required
-              ></textarea>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </section>
+        <div className="profile">
+          <img
+            src={shahid}
+            alt="Your Name"
+            height="200px"
+            width="200px"
+          />
+          <h2 className="nam">MAHAR SHAHID IQBAL JHAWARI</h2>
+          <strong>
+            <p>
+              WEB DISGNER, DEVELOPER, FREELANCER YOUTUBER AND GRAPHIC DISGNER.
+            </p>
+          </strong>
+          <p>
+            Phone: <a href="tel:+923447014153">+923447014153</a>
+          </p>
+        </div>
+      </main>
+      <main className="abo">
+        <div className="profile">
+          <img
+            src={muutafa}
+            alt="Your Name"
+            height="200px"
+            width="200px"
+          />
+          <h2 className="nam">GULAM MUSTAFA</h2>
+          <strong>
+            <p>
+              WEB DISGNER, DEVELOPER, FREELANCER YOUTUBER AND GRAPHIC DISGNER.
+            </p>
+          </strong>
+          <p>
+            Phone: <a href="tel:+923496807311">+923496807311</a>
+          </p>
+        </div>
+      </main>
+      <main className="abo">
+        <div className="profile">
+          <img src={ali} alt="Your Name" height="200px" width="200px" />
+          <h2 className="nam">ALI JAN</h2>
+          <strong>
+            <p>
+              WEB DISGNER, DEVELOPER, FREELANCER YOUTUBER AND GRAPHIC DISGNER.
+            </p>
+          </strong>
+          <p>
+            Phone: <a href="tel:+923260340578">+923260340578</a>
+          </p>
+        </div>
       </main>
       <Footer />
     </div>
